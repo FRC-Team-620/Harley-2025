d81172afcc91702228a1e327fbbbcc55d5b7ae04
Developer 1 <dev1@warbots.onmicrosoft.com>
2025-12-04 18:41:58 -0500

    Anti-Tip Code Complete
    Debug Anti-Tip roll wrapping issue.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
@@ -32,6 +33,7 @@ import org.littletonrobotics.urcl.URCL;
 public class Robot extends LoggedRobot {
   private Command autonomousCommand;
   private RobotContainer robotContainer;
+  private Spark uptimeMeter = new Spark(1);
 
   public Robot() {
     // Record metadata
@@ -109,7 +111,9 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+    uptimeMeter.set(0);
+  }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
@@ -140,7 +144,9 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    uptimeMeter.set(-1);
+  }
 
   /** This function is called once when test mode is enabled. */
   @Override
